1f8aaf26ec89a921e23a164717a3710dfe515ec6
andersongulartew <andersongularte1993@gmail.com>
2023-12-18 17:01:26 -0300

    .

---
+++ seção_3_exercicios_sobre_tipos_de_dados/exercicio_4/script.js
@@ -0,0 +1,8 @@
+// Escreva tres comparações cm operadores logicos
+// com and, or e not
+
+console.log(3 == 3 && 4 > 1);
+
+console.log(4 <= 2 || "anderson" == "pedro");
+
+console.log(!(2 === 2));
---
+++ seção_3_exercicios_sobre_tipos_de_dados/exercicio_5/script.js
@@ -0,0 +1,3 @@
+//faça uma operação que emita Nan no console do navegador
+
+console.log(415 * "não é numero")
