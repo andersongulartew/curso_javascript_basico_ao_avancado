52938871cac1b43e2ca9491e5f8c2f4c44753b40
andersongulartew <andersongularte1993@gmail.com>
2023-12-18 17:30:42 -0300

    .

--- seção_3_exercicios_sobre_tipos_de_dados/exercicio_5/script.js
+++ seção_3_exercicios_sobre_tipos_de_dados/exercicio_5/script.js
@@ -1,3 +1,3 @@
-//faça uma operação que emita Nan no console do navegador
+//faça uma operação que emita NaN no console do navegador
 
 console.log(415 * "não é numero")
---
+++ seção_5_exercicios/exercicio.10.js
@@ -0,0 +1,11 @@
+// armazene a velocidade de um carro em uma variavel;
+// faça estrutura if/else que verificar se esta acima da velocidade;
+// se estiver acima ou abaixo exiba mensagens
+
+var velocidade = 80
+
+if (velocidade <= 80) {
+    console.log("você esta na velocidade permitida.")
+} else {
+    console.log("você esta acima velocidade permitida,recebera uma multa .")
+}
