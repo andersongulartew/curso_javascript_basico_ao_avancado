73f92855d1e2cbfde283c8fa8be619951ed4c77c
andersongulartew <andersongularte1993@gmail.com>
2023-12-20 15:10:31 -0300

    .

--- secao_5_exercicios/exercicio_11.js
+++
@@ -1,9 +0,0 @@
-
-
-//  * ATIVIDADE *
-
-// faça uma estrutura if/else para verificar se um usuario pode dirigir;
-// armazene em variaveis informação sobre o usuario: idade, se tem CNH;
-// se a idade for maior que 18  e nao possuir CNH, exiba mensagem;
-// se a idade for maior que 18 e tem CNH, exiba uma mensagem
-// se não tiver 18 e nem CNH, exiba uma mensagem;
